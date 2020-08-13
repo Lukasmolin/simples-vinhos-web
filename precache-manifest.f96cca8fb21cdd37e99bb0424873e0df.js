d8ede0797f35981c0f512a17090ca14ce8657af9
Lucas <lukasmolin@gmail.com>
2020-08-13 06:25:54 -0300

    Updates

---
+++ precache-manifest.f96cca8fb21cdd37e99bb0424873e0df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb59f4a91fb9918bb3e52389823312b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81fd8ee6d165db39bf4b",
+    "url": "/static/css/main.488d6120.chunk.css"
+  },
+  {
+    "revision": "e8086e648f3fc2bc0929",
+    "url": "/static/js/2.234a36f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.234a36f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81fd8ee6d165db39bf4b",
+    "url": "/static/js/main.3283b1fb.chunk.js"
+  },
+  {
+    "revision": "bcf7573bbd6f064c5ca9",
+    "url": "/static/js/runtime-main.c8b7f8e7.js"
+  },
+  {
+    "revision": "b26d6fcdcbaf2c5192ae24d6b7e4fefb",
+    "url": "/static/media/SimplesLogo.b26d6fcd.png"
+  }
+]);
