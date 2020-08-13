de4a409e5ddbc8696153d2bacacebb2bc8bef5fe
Lucas <lukasmolin@gmail.com>
2020-08-13 04:44:42 -0300

    Starts header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../resources/imgs/SimplesLogo.png';
+import './Header.css';
 
 export interface HeaderProps {
     menu: {
@@ -12,10 +14,21 @@ export interface HeaderProps {
 
 export const Header : React.FC <HeaderProps> = (props: HeaderProps) => {
     const menu = props.menu.map(val => {
-        return <Link to={val.to}>{val.label}</Link>;
+        return <Link
+            key={val.to+val.label}
+            to={val.to}
+            children={val.label}
+        />;
     });
 
-    return <header>
+    return <header className="header-comp">
+        <div className="logoWrapper">
+            <img 
+                className="logoImg"
+                src={logo}
+                alt="Simples Logo">
+            </img>
+        </div>
         <nav>
             {menu}
         </nav>
