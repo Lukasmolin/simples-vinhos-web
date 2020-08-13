50f92cb5cb0a8641eeece3ccde8d5a79422a54e7
Lucas <lukasmolin@gmail.com>
2020-08-13 02:21:04 -0300

    HomePage became functional component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,36 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header, { HeaderProps } from './components/Header';
 import HomePage from './pages/HomePage';
-import Service from './service/Service';
 
-const navHeader: HeaderProps = {
-    menu: [
-        { to: 'testeA', label: 'testeA' },
-        { to: 'teste2', label: 'teste2' },
-        { to: 'teste3', label: 'teste3' },
-        { to: 'teste4', label: 'teste4' },
-    ],
-    logo: {
-        img: ''
-    },
-    text: {
-        content: '',
-        font: {
-            type: ''
-        }
-    }
-};
-
-export default function Routes(){   
-    const homePage = new HomePage(navHeader, new Service());
-
+export default function Routes(){
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" component={homePage.render}/>
+                <Route path="/" component={HomePage}/>
             </Switch>
         </BrowserRouter>
     );
-
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,31 +2,15 @@ import React from 'react';
 import Header, { HeaderProps } from '../components/Header';
 import Service from '../service/Service';
 
-export default class HomePage extends React.Component {
+const service = new Service();
 
-    constructor(private header: HeaderProps, private service: Service) {
-        super({});
-        this.render = this.render.bind(this);
-    }
-
-    private generateRender() {
-        const info = this.service.author;
-        return (
-            <div>
-                {Header(this.header)}
-                <main>
-                    {JSON.stringify(info)}
-                </main>
-            </div>
-        )
-    }
-
-    public render() {
-        return this.generateRender();
-    }
-
-
-
-
-
+const HomePage: React.FC = () => {
+    return <>
+        <Header menu={service.menu} logoImg={''} textContent={ 'Simples Vinhos' } />
+        <main>
+            {JSON.stringify(service.wines)}
+        </main>
+    </>;
 }
+
+export default HomePage;
