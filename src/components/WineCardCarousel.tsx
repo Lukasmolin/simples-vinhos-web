fc5b89ef77e8d4e3c53153bbbc91b598975c87d2
Lucas <lukasmolin@gmail.com>
2020-08-19 15:40:50 -0300

    Integrates basic carousel to page

--- src/components/WineCardCarousel.tsx
+++ src/components/WineCardCarousel.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import WineCard from './WineCard';
 import './WineCardCarousel.css';
 
 export interface WineCardCarouselProps {
-    /*
-    WineList
-    NumberPerPage
-    */
+    wineList: {
+        imageSrc: string;
+        title: string;
+        content: string;
+        linkTo: string;
+    }[];
 }
 
 const WineCardCarousel : React.FC<WineCardCarouselProps> = (props: WineCardCarouselProps) => {
-    return <div></div>
+    const { wineList } = props;
+    const wine = wineList[0];
+    
+    return <div className="wineCardCarousel-comp">
+        <WineCard
+            imageSrc={wine.imageSrc}
+            title={wine.title}
+            content={wine.content}
+        />
+    </div>;
 }
 
 export default WineCardCarousel;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import Header from '../components/Header';
 import Service from '../service/Service';
-import HomePageService from '../service/HomePageService';
 import WineCard from '../components/WineCard';
+import WineCardCarousel from '../components/WineCardCarousel';
 
 const service = new Service();
-const homePageService = new HomePageService();
 
 const HomePage: React.FC = () => {
+    const content = "O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele.";
+    const title="Vinho Santo Gole";
+
     return <>
         <Header menu={service.menu} logoImg={service.logoImg} />
         <main>
-            <WineCard 
-                imageSrc={service.logoImg}
-                title="Vinho Santo Gole"
-                content="O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele. O vinho verdadeiramente santificado d'Ele."    
+            <WineCardCarousel
+                wineList={[{ content, title, imageSrc: service.logoImg, linkTo: ""  }]}  
             />
         </main>
     </>;
