0d44c3e47f6c121cb127cbcf4b9c4b75850fe2ea
Lucas <lukasmolin@gmail.com>
2020-08-19 15:15:18 -0300

    Creates WineCardCarousel component

---
+++ src/components/WineCardCarousel.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './WineCardCarousel.css';
+
+export interface WineCardCarouselProps {
+    /*
+    WineList
+    NumberPerPage
+    */
+}
+
+const WineCardCarousel : React.FC<WineCardCarouselProps> = (props: WineCardCarouselProps) => {
+    return <div></div>
+}
+
+export default WineCardCarousel;
