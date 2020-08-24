92547b6d8822b9f265985986670b6df8614fd220
Lucas <lukasmolin@gmail.com>
2020-08-24 01:11:56 -0300

    PriceList final styling

--- src/components/PriceList.tsx
+++ src/components/PriceList.tsx
@@ -13,16 +13,20 @@ export interface PriceListSectionProps {
 export const PriceListSection: React.FC<PriceListSectionProps> = (props) => {
     return <div className="priceListSectionContainer">
         <div className="priceListSectionTitle">
-            <h1>{props.title}</h1>
+            <span className="headerDivider" >
+            </span>
+            <span className="titleContainer">
+                <h1>{props.title}</h1>
+            </span>
         </div>
         <div className="priceListItemsContainer">
         {props.items.map(item => {
             return <div className="priceListItem">
-                <span>{item.name}</span>
-                <span>R$ {item.price}</span>
+                <span className="itemName">{item.name}</span>
+                <span className="itemPrice">R$ {item.price}</span>
             </div>;
         })}
-        </div>                
+        </div>
     </div>;
 }
 
